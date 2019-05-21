02b5186dc4dbe3f0ee6942d9de62d42a427b54db
Joe Pena <joe@pop-os.localdomain>
2019-05-20 20:10:25 -0600

    Create Todos component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'atomic-angular-demo';
+  title = 'Atomic Angular App';
 }
