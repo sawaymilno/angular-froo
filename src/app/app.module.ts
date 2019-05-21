acb1d8bcf00cfc39a6306b7c07bb5d7737a0c33c
Joe-Pena <joepena@protonmail.com>
2019-05-21 15:54:31 -0600

    Initialize In Memory Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
+import { InMemoryDataService } from './services/InMemoryData/in-memory-data.service'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { TodosComponent } from './ecosystems/todos/todos.component'
 
-import { FormsModule } from '@angular/forms';
-import { TodoDetailComponent } from './organisms/todo-detail/todo-detail.component';
-import { MessagesComponent } from './organisms/messages/messages.component';
+import { FormsModule } from '@angular/forms'
+import { TodoDetailComponent } from './organisms/todo-detail/todo-detail.component'
+import { MessagesComponent } from './organisms/messages/messages.component'
 import { DashboardComponent } from './ecosystems/dashboard/dashboard.component'
 
 @NgModule({
@@ -21,7 +24,9 @@ import { DashboardComponent } from './ecosystems/dashboard/dashboard.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/InMemoryData/in-memory-data.service.ts
@@ -0,0 +1,10 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api'
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService {
+
+  constructor() { }
+}
